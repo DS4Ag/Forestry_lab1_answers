61212f9061989f31203927367dc743081e1fa6bf
Luis Vargas Rojas <luizvargaz@hotmail.com>
2025-03-23 19:04:45 -0400

    Main integration

---
+++ main.R
@@ -0,0 +1,8 @@
+setwd("C:/Users/luizv/OneDrive_Purdue/OneDrive - purdue.edu/Spring 2025/AGR 33300/Forestry")
+
+source("1_data_integration.R")
+source("2_calculations _EF_BA_TPA.R")
+source("3_summarize_data.R")
+source("4_calculations_biomass.R")
+source("5_calculations_sps_dom_rich.R")
+source("6_final_summary.R")
